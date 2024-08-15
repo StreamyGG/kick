91b2a96c0c327bbc997642e6ce79d04a58b1e80b
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-08-15 00:43:08 -0500

    Get Kick working temp

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,18 +1,68 @@
-import { Events, Kient } from 'kient'
-import { EMAIL, PASSWORD } from '../config';
+import puppeteer from 'puppeteer';
+import { Events, Kient } from 'kient';
+import { EMAIL, USERNAME, PASSWORD } from '../config';
+import { createClient } from 'redis';
+
+const redisClient = createClient();
 
 const init = async () => {
-    const client = await Kient.create()
+    await redisClient.connect();
 
-    const channel = await client.api.channel.getChannel("uddernonsense")
- 
-    // await channel.connectToChatroom()
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
 
-    // client.on(Events.Chatroom.Message, (messageInstance) => {
-    //   const message = messageInstance.data
-    //   console.log(`${message.sender.username}: ${message.content}`)
-    // })
+    await page.goto('https://kick.com', { waitUntil: 'networkidle0' });
 
+    const isLoggedOut = await page.$('input[name="email"]') !== null;
+
+    if (isLoggedOut) {
+        console.log("Login form detected, logging in...");
+        await page.type('input[name="email"]', EMAIL!);
+        await page.type('input[name="password"]', PASSWORD!);
+
+        await Promise.all([
+            page.click('button[type="submit"]'),
+            page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        ]);
+
+        console.log("Login successful.");
+    } else {
+        console.log("Already logged in.");
+    }
+
+    await browser.close();
+
+    const client = await Kient.create();
+    const channel = await client.api.channel.getChannel("xqc");
+
+    await channel.connectToChatroom();
+
+    client.on(Events.Chatroom.Message, async (messageInstance) => {
+        const message = messageInstance.data;
+
+        const data = {
+            streamy: {
+                platform: "kick",
+                account: {
+                    username: USERNAME,
+                }
+            },
+            sender: {
+                id: message.sender.id,
+                username: message.sender.slug,
+                color: message.sender.identity.color,
+                badges: message.sender.identity.badges,
+                displayName: message.sender.username,
+            },
+            message: { 
+                content: message.content,
+                id: message.id,
+                createdAt: message.created_at,
+            }
+        };
+
+            await redisClient.publish('chat', JSON.stringify(data));
+    });
 };
 
 export default { init };
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,4 @@ require("dotenv").config();
 
 export const EMAIL = process.env.KICK_EMAIL;
 export const PASSWORD = process.env.KICK_PASSWORD;
+export const USERNAME = process.env.KICK_USERNAME;
